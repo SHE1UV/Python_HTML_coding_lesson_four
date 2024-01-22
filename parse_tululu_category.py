8c6b696d6961cd0fc5fa1b90528b21353bf6055e
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2024-01-22 16:21:48 +0300

    Исправил опечатку

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,7 +8,7 @@ import requests
 from urllib.parse import unquote, urljoin, urlsplit
 from bs4 import BeautifulSoup
 
-from tools import check_for_redirect,parse_book_page,download_txt,download_image
+from save_tools import check_for_redirect,parse_book_page,download_txt,download_image
 
 
 def main():
